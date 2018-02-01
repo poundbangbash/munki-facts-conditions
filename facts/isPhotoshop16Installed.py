8e2fa36e28b1bdb35546ffcb1292e2fe15d5ad99
eholtam <(none)>
2018-02-01 08:04:55 -0600

    Removed old bash code from isPhotoshop16Installed

--- facts/isPhotoshop16Installed.py
+++ facts/isPhotoshop16Installed.py
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-plistLocation="/Library/Managed Installs/ConditionalItems"
-applicationLocation="/Applications/Adobe Photoshop CC 2015/Adobe Photoshop CC 2015.app/Contents/MacOS/Adobe Photoshop CC 2015"
-prefName=isPhotoshop16Installed
-
-#Verify a user is logged in
-if [ -e "${applicationLocation}" ]
-then
-	defaults write "${plistLocation}" "${prefName}" -bool true
-else
-	defaults write "${plistLocation}" "${prefName}" -bool false
-fi
-
 import os.path
 
 def fact():
